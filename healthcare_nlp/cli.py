658313a193e48b42cc14e4fc6f66e950484ce05d
ChristinaKragesten <163416813+ChristinaKragesten@users.noreply.github.com>
2024-04-26 11:57:20 +0000

    create class to summarize text

--- healthcare_nlp/cli.py
+++ healthcare_nlp/cli.py
@@ -1,6 +1,7 @@
 import click
 
 from healthcare_nlp.setup_data import SetUpData
+from healthcare_nlp.src_code.summary import TextSummary
 
 @click.group()
 @click.option(
@@ -19,43 +20,7 @@ def cli(ctx, cfg_filepath: str, txt_filepath: str):
     ctx.obj = SetUpData(config_filepath=cfg_filepath, text_filepath=txt_filepath)
 
 @cli.command()
-@click.option("--val", default=1)
+@click.option("--above_avg_factor", default=1.2)
 @click.pass_context
-def summarize(ctx, val):
-    from nltk.corpus import stopwords
-    from nltk.tokenize import word_tokenize, sent_tokenize
-    stop_words = set(stopwords.words("english"))
-    words = word_tokenize(ctx.obj.text)
- 
-    frequency_table = dict()
-    for word in words:
-        word = word.lower()
-        if word in stop_words:
-            continue
-        if word in frequency_table:
-            frequency_table[word]+=1
-        else:
-            frequency_table[word] = 1
-    sentences = sent_tokenize(ctx.obj.text)
-    sentences_value = dict()
-    for sentence in sentences:
-        for word, freq in frequency_table.items():
-            if word in sentence.lower():
-                if sentence in sentences_value:
-                    sentences_value[sentence] += freq
-                else:
-                    sentences_value[sentence] = freq
-
-    sum_values = 0
-    for sentence in sentences_value:
-        sum_values += sentences_value[sentence]
-    
-    average = int(sum_values / len(sentences_value))
-
-    summary=""
-    for sentence in sentences:
-        if (sentence in sentences_value) and (sentences_value[sentence]>(1.2 * average)):
-            summary += " " + sentence
-    print(summary)
-
-
+def summarize(ctx, above_avg_factor):
+    print(TextSummary(ctx.obj.text, above_avg_factor).summarize())
